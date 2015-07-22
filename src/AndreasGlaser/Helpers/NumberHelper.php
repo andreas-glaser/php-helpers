513fd245243d92123caa5fd6acadd06f397bbd7f
Andreas Glaser <andreas.c.glaser@gmail.com>
2015-07-22 15:58:22 +0700

    Added number helper

---
+++ src/AndreasGlaser/Helpers/NumberHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AndreasGlaser\Helpers;
+
+/**
+ * Class NumberHelper
+ *
+ * @package AndreasGlaser\Helpers
+ * @author  Andreas Glaser
+ */
+class NumberHelper
+{
+    /**
+     * @param $number
+     * @return string
+     * @author Andreas Glaser
+     */
+    public static function ordinal($number)
+    {
+        if ($number % 100 > 10 && $number % 100 < 14) {
+            return 'th';
+        }
+
+        switch ($number % 10) {
+            case 1:
+                return 'st';
+            case 2:
+                return 'nd';
+            case 3:
+                return 'rd';
+            default:
+                return 'th';
+        }
+    }
+}

489e1b7ba70e20f11fbbe59465f7c1f552006d41
Andreas Glaser <andreas.c.glaser@gmail.com>
2015-12-07 18:23:10 +0800

    Improved JsonHelper

--- src/AndreasGlaser/Helpers/JsonHelper.php
+++ src/AndreasGlaser/Helpers/JsonHelper.php
@@ -2,55 +2,32 @@
 
 namespace AndreasGlaser\Helpers;
 
+/**
+ * Class JsonHelper
+ *
+ * @package AndreasGlaser\Helpers
+ * @author  Andreas Glaser
+ */
 class JsonHelper
 {
+
     /**
+     * Validates JSON input.
+     *
      * @param $string
      *
      * @return bool
-     *
      * @author Andreas Glaser
-     * @source http://andreas.glaser.me/2013/05/27/php-check-if-string-is-json/
      */
     public static function isValid($string)
     {
-        // make sure provided input is of type string
-        if (!is_string($string)) {
-            return false;
+        if (is_int($string) || is_float($string)) {
+            return true;
         }
 
-        // trim white spaces
-        $string = trim($string);
-
-        // get first character
-        $firstChar = substr($string, 0, 1);
-
-        // get last character
-        $lastChar = substr($string, -1);
-
-        // check if there is a first and last character
-        if (!$firstChar || !$lastChar) {
-            return false;
-        }
-
-        // make sure first character is either { or [
-        if ($firstChar !== '{' && $firstChar !== '[') {
-            return false;
-        }
-
-        // make sure last character is either } or ]
-        if ($lastChar !== '}' && $lastChar !== ']') {
-            return false;
-        }
-
-        // let's leave the rest to PHP.
-        // try to decode string
         json_decode($string);
 
-        // check if error occurred
-        $isValid = json_last_error() === JSON_ERROR_NONE;
-
-        return $isValid;
+        return json_last_error() === JSON_ERROR_NONE;
     }
 
     /**
@@ -59,7 +36,6 @@ class JsonHelper
      * @param $string
      *
      * @return string
-     *
      * @author Andreas Glaser
      */
     public static function encodeForJavaScript($string)
---
+++ tests/AndreasGlaser/Helpers/Tests/JsonHelperTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AndreasGlaser\Helpers\Tests;
+
+use AndreasGlaser\Helpers\JsonHelper;
+
+/**
+ * Class JsonHelperTest
+ *
+ * @package AndreasGlaser\Helpers\Tests
+ * @author  Andreas Glaser
+ */
+class JsonHelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @author Andreas Glaser
+     */
+    public function testIsValid()
+    {
+        $this->assertTrue(JsonHelper::isValid(123));
+        $this->assertTrue(JsonHelper::isValid(123.43));
+        $this->assertTrue(JsonHelper::isValid(true));
+        $this->assertTrue(JsonHelper::isValid(false));
+        $this->assertTrue(JsonHelper::isValid(null));
+        $this->assertTrue(JsonHelper::isValid('true'));
+        $this->assertTrue(JsonHelper::isValid('false'));
+        $this->assertTrue(JsonHelper::isValid('null'));
+        $this->assertTrue(JsonHelper::isValid('"abc"'));
+        $this->assertFalse(JsonHelper::isValid("'abc'"));
+        $this->assertFalse(JsonHelper::isValid('00 11 22'));
+        $this->assertTrue(JsonHelper::isValid('[1,2,3, null, true, false, "abc"]'));
+        $this->assertTrue(JsonHelper::isValid('{"a":{},"b":false}'));
+        $this->assertTrue(JsonHelper::isValid('[1,2,3,4]'));
+        $this->assertTrue(JsonHelper::isValid('{"b":{},"c":[{},[1,2,3,true,false,null]]}'));
+    }
+}

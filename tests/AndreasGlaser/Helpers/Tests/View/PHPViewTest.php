2bfae52e5a1b98e23a000282edb5c1a617cc65bb
Andreas Glaser <andreas.c.glaser@gmail.com>
2016-12-29 13:17:25 +0700

    Completed PHPView helper

---
+++ tests/AndreasGlaser/Helpers/Tests/View/PHPViewTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AndreasGlaser\Helpers\Tests\View;
+
+use AndreasGlaser\Helpers\HtmlHelper;
+use AndreasGlaser\Helpers\Tests\BaseTest;
+use AndreasGlaser\Helpers\View\PHPView;
+
+/**
+ * Class PHPViewTest
+ *
+ * @package AndreasGlaser\Helpers\Tests\View
+ * @author  Andreas Glaser
+ */
+class PHPViewTest extends BaseTest
+{
+    /**
+     * @author Andreas Glaser
+     */
+    public function test()
+    {
+        PHPView::setGlobal('name', 'PHPView');
+
+        $view = new PHPView(__DIR__ . DIRECTORY_SEPARATOR . 'view.html.php');
+        $view->set('link', HtmlHelper::a('/test', 'Test Url'));
+        $view->set('rating', 5);
+
+        $this->assertEquals('<p>PHPView</p>' . PHP_EOL . '<p><a href="/test" >Test Url</a> (Rating: 5)</p>', $view->render());
+    }
+}
---
+++ tests/AndreasGlaser/Helpers/Tests/View/view.html.php
@@ -0,0 +1,2 @@
+<p><?php print $global['name']; ?></p>
+<p><?php print $data['link']; ?> (Rating: <?php print $rating; ?>)</p>

0c708d9004a5f0fe6cfe9fa60af1f80f5908747b
Andreas Glaser <andreas.c.glaser@gmail.com>
2017-01-19 00:24:03 +0800

    Fixed UrlHelper + Added RequestHelper

---
+++ src/AndreasGlaser/Helpers/Http/RequestHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AndreasGlaser\Helpers\Http;
+
+/**
+ * Class RequestHelper
+ *
+ * @package AndreasGlaser\Helpers\Http
+ * @author  Andreas Glaser
+ */
+class RequestHelper
+{
+    /**
+     * @return bool
+     * @author Andreas Glaser
+     */
+    public static function isCli()
+    {
+        return isset($_SERVER['REQUEST_METHOD']);
+    }
+
+    /**
+     * @return bool
+     * @author Andreas Glaser
+     */
+    public static function isHttps()
+    {
+        return isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
+    }
+}
+
+

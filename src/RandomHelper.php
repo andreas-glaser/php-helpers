fcaffd02e16854d59eade798854438056c22441c
Andreas Glaser <andreas.glaser@pm.me>
2019-11-25 09:27:11 +0100

    Created RandomHelper::uniqid()

--- src/RandomHelper.php
+++ src/RandomHelper.php
@@ -16,4 +16,25 @@ class RandomHelper
     {
         return rand(0, 1) === 1;
     }
+
+    /**
+     * @param string $prefix
+     *
+     * @return string
+     * @throws \Exception
+     * @source @source https://www.php.net/manual/en/function.uniqid.php#120123
+     */
+    public static function uniqid(string $prefix = ''): string
+    {
+
+        if (true === function_exists('random_bytes')) {
+            $bytes = random_bytes(7);
+        } elseif (true === function_exists('openssl_random_pseudo_bytes')) {
+            $bytes = openssl_random_pseudo_bytes(7);
+        } else {
+            throw new \Exception('No cryptographically secure random function available');
+        }
+
+        return $prefix . mb_substr(bin2hex($bytes), 0, 13);
+    }
 }
--- tests/Tests/RandomHelperTest.php
+++ tests/Tests/RandomHelperTest.php
@@ -11,10 +11,25 @@ use AndreasGlaser\Helpers\RandomHelper;
  */
 class RandomHelperTest extends BaseTest
 {
-    /**
-     */
+
     public function testTrueFalse()
     {
         $this->assertInternalType('boolean', RandomHelper::trueFalse());
+    }
+
+    public function testUniqid()
+    {
+        $this->assertEquals(13, strlen(RandomHelper::uniqid()));
+
+        $this->assertStringStartsWith('my_prefix_', RandomHelper::uniqid('my_prefix_'));
+        $this->assertEquals(23, strlen(RandomHelper::uniqid('my_prefix_')));
+
+        $uniqueIds = [];
+
+        for ($i = 1; $i <= 1000000; $i++) {
+            $uniqueIds[] = RandomHelper::uniqid();
+        }
+
+        $this->assertEquals(count($uniqueIds), count(array_unique($uniqueIds)));
     }
 }
